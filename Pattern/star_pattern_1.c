b025a06d9d685fc346b03f33446ab2a95c8b2563
Mayank Khurmai <mayankkhurmai8@gmail.com>
2020-11-03 22:58:41 +0530

    Create star_pattern_1.c

---
+++ Pattern/star_pattern_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int rc;
+    printf("Enter the Number of Rows : ");
+    scanf("%d",&rc);
+    printf("\n");
+    for(int i=0; i<rc; i++)
+    {
+        for(int j=0; j<rc; j++)
+        {
+            printf("%d", rc);
+        }
+        printf("\n");
+    }
+    return 0;
+}
